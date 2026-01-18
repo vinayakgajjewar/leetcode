f8d493e578681dfc99c0a7603468b9721ad55743
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2026-01-17 17:41:21 -0800

    better solution

--- 128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <set>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -14,7 +15,9 @@ public:
 
     /*
      * This is the naive solution that runs in O(N log N) because it uses a
-     * sorting algorithm.
+     * sorting algorithm. This solution does give us the basic pattern for finding
+     * the length of the longest consecutive sequence once we have a sorted unique
+     * vector, though.
      */
     int longestConsecutive_naive(vector<int> &nums) {
         int n = (int) nums.size();
@@ -36,5 +39,30 @@ public:
             max_count = max(c, max_count);
         }
         return max_count;
+    }
+
+    /*
+     * This is the better solution that runs in O(N) because the input array does
+     * not need to be sorted. The intuition here is that we improve upon the naive
+     * solution by, for each unique number that is the start of a new sequence,
+     * keep looking for the number that is 1 greater, then 2 greater, and so on.
+     * This solution also does not need to store an entire array of count values
+     * and just keeps track of the longest one seen so far.
+     */
+    int longestConsecutive_better(vector<int> &nums) {
+        int n = (int) nums.size();
+        if (n == 0) return 0;
+        unordered_set<int> unique(nums.begin(), nums.end());
+        int count = 0;
+        int longest_count = 0;
+        for (int num: unique) {
+            if (unique.contains(num - 1)) continue;
+            count = 0;
+            while (unique.contains(num + count)) {
+                count++;
+            }
+            if (longest_count < count) longest_count = count;
+        }
+        return longest_count;
     }
 };
