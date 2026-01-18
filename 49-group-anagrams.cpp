95d3c91190905f0dbadff036ebc319932f9f1b84
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2026-01-18 09:35:20 -0800

    did group anagrams

--- 49-group-anagrams.cpp
+++ 49-group-anagrams.cpp
@@ -6,24 +6,23 @@ using namespace std;
 
 class Solution {
 public:
+
+    /*
+     * The intuition here is to construct an unordered map where the keys are the
+     * sorted versions of strings and the values are all the permutations of that
+     * key that appear in the input.
+     *
+     * Notice how to sort a string in C++!
+     */
     static vector<vector<string>> groupAnagrams(vector<string> &strs) {
-        /*
-         * Making a vector of frequency tables is the wrong approach here. We
-         * can make a hash table where keys are the sorted versions of anagrams
-         * and the values are the unsorted versions we've seen.
-         */
         unordered_map<string, vector<string>> anagrams;
         for (string &s: strs) {
             string sorted = s;
-
-            /*
-             * This is a nifty way to sort the characters in a string! std::sort
-             * runs in O(n log_2 n).
-             */
             sort(sorted.begin(), sorted.end());
             anagrams[sorted].push_back(s);
         }
         vector<vector<string>> grouped_anagrams;
+        grouped_anagrams.reserve(anagrams.size());
         for (auto &[key, value]: anagrams) {
             grouped_anagrams.push_back(value);
         }
